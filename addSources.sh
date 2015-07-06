31d251c4f9231468d038a292758dcf0f6e80bae8
Michael Xavier <michael@michaelxavier.net>
2015-07-06 15:54:17 -0700

    Add submodules

---
+++ addSources.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+cabal sandbox init
+cabal sandbox add-source deps/retry
+cabal sandbox add-source deps/multi-compression
+cabal sandbox add-source deps/katip/katip
+cabal sandbox add-source deps/aws-utils

b2d64fba639fcf0be38f1f7ad41142ab482b4c5e
Michael Xavier <michael@michaelxavier.net>
2015-11-13 10:42:27 -0800

    Don't vendor retry anymore

--- addSources.sh
+++ addSources.sh
@@ -2,7 +2,6 @@
 set -e
 
 cabal sandbox init
-cabal sandbox add-source deps/retry
 cabal sandbox add-source deps/multi-compression
 cabal sandbox add-source deps/katip/katip
 cabal sandbox add-source deps/aws-utils
